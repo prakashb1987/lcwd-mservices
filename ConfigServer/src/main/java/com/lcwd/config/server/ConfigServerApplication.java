519dec563ae25b2dbed5722ddb045dd548cabec1
bhanuprakashh <prakashb@vmware.com>
2022-12-24 14:20:32 +0530

    Add Config Server

--- ConfigServer/src/main/java/com/lcwd/config/server/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/lcwd/config/server/ConfigServerApplication.java
@@ -1,16 +1,17 @@
-package com.lcwd.rating;
+package com.lcwd.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-
-public class RatingServiceApplication {
+@EnableConfigServer
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RatingServiceApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
